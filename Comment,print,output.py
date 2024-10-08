81efaf89a2dd6f8dcb6be2cb7a568975dd9fa20c
Refinaldii <refinaldi9786@gmail.com>
2024-10-08 10:37:28 +0700

    print("Muhammad Refinaldi Alif")
    print("I am learning Python basics.")
    
    user_name = input("What is your name? ")  #Dapatkan input dari pengguna dan simpan di variabel user_name
    
    print("Hello, " + user_name + "!")  # Print the greeting with the user's name
    
    age = input("How old are you? ")  # Dapatkan input dari pengguna dan simpan di variabel age
    
    print(age)  # Print the age

---
+++ Comment,print,output.py
@@ -0,0 +1,20 @@
+# Print nama buat ke konsole
+print("Muhammad Refinaldi Alif")  
+
+# Print kalimat "I am learning Python basics." di line baru
+print("I am learning Python basics.")
+
+# bertanya ke user siapa nama mereka 
+# Fungsi input() digunakan untuk mendapatkan input dari pengguna, dan prompt-nya adalah 'Siapa nama Anda?
+user_name = input("What is your name? ")  #Dapatkan input dari pengguna dan simpan di variabel user_name
+
+# salam user dengan  "Hello, [Name]!"
+# Fungsi print() digunakan untuk mencetak salam, dan nama pengguna dimasukkan menggunakan format string.
+print("Hello, " + user_name + "!")  # Print the greeting with the user's name
+
+# Kode berikut meminta pengguna untuk memasukkan usianya, kemudian mencetaknya.
+# bertanya ke user berapa umur mereka 
+age = input("How old are you? ")  # Dapatkan input dari pengguna dan simpan di variabel age
+
+#print umur user
+print(age)  # Print the age
